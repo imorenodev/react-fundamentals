06ee7efb35142bdc2ba24206f2b704c00a8fff51
imorenodev <imorenodev@gmail.com>
2016-03-08 17:52:23 -0500

    Passing props with this.props.children. Added boostrap

--- App.js
+++ App.js
@@ -1,56 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//Stateless function Component:
-//const App = () => <h1>Hello World!</h1>
-
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      red: 0,
-      green: 0,
-      blue: 0
-    }
-    this.update = this.update.bind(this)
-  }
-  update(e){
-    this.setState({
-      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
-      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
-      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
-    })
-  }
   render() {
     return (
       <div>
-        <Slider ref="red" update={this.update} />
-        {this.state.red}
-        <hr />
-        <Slider ref="green" update={this.update} />
-        {this.state.green}
-        <hr />
-        <Slider ref="blue" update={this.update} />
-        {this.state.blue}
-        <hr />
+        <Button>I <Heart/> React</Button>
       </div>
     );
   }
 }
 
-class Slider extends React.Component{
+class Button extends React.Component {
   render() {
     return (
-      <div>
-        <input ref="inp"
-          type="range"
-          min="0"
-          max="255"
-          onChange={this.props.update} />
-      </div>
-    );
+      <button className="btn btn-lg">{this.props.children}</button>
+    )
   }
 }
 
+const Heart = () => <span className="glyphicon glyphicon-heart"></span>
 
 export default App;
