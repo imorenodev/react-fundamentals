124d403a25c24d802e6112e6bbc9d7633495a0be
imorenodev <imorenodev@gmail.com>
2016-03-08 17:22:15 -0500

    Managing state with refs

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 //Stateless function Component:
 //const App = () => <h1>Hello World!</h1>
@@ -6,31 +7,50 @@ import React from 'react';
 class App extends React.Component {
   constructor(){
     super();
-    this.state = { txt: '' }
+    this.state = {
+      red: 0,
+      green: 0,
+      blue: 0
+    }
     this.update = this.update.bind(this)
   }
   update(e){
     this.setState({
-      txt: e.target.value
+      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
     })
   }
   render() {
     return (
       <div>
-        <Widget update={this.update} txt={this.state.txt} />
+        <Slider ref="red" update={this.update} />
+        {this.state.red}
+        <hr />
+        <Slider ref="green" update={this.update} />
+        {this.state.green}
+        <hr />
+        <Slider ref="blue" update={this.update} />
+        {this.state.blue}
+        <hr />
       </div>
     );
   }
 }
 
-const Widget = (props) => {
-  return (
-    <div>
-      <input type="text"
-        onChange={props.update} />
-      <h1>{props.txt}</h1>
-    </div>
-  );
+class Slider extends React.Component{
+  render() {
+    return (
+      <div>
+        <input ref="inp"
+          type="range"
+          min="0"
+          max="255"
+          onChange={this.props.update} />
+      </div>
+    );
+  }
 }
+
 
 export default App;
