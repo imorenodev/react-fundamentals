f755eccd025522098db5d2b8cd831acc0de85d89
imorenodev <imorenodev@gmail.com>
2016-03-08 15:23:48 -0500

    Adding props, propTypes, and defaultProps to App.js

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Stateless function Component:
+//const App = () => <h1>Hello World!</h1>
 
 class App extends React.Component {
   render() {
+    let { text, cat } = this.props;
     return (
-      <div>Hello</div>
+      <div>
+        <h1>{cat}. {text}</h1>
+      </div>
     )
   }
 }
+App.propTypes = {
+  text: React.PropTypes.string.isRequired,
+  cat: React.PropTypes.number.isRequired
+}
+App.defaultProps = {
+  text: 'this is the default text',
+  cat: 999
+}
+
+ReactDOM.render(
+  <App text="this is the props text" cat={5} />,
+  document.getElementById('app')
+);
+
 
 export default App;
