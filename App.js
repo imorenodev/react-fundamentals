c9e7615083983506f3265f595aa2ceb592e7b2fa
imorenodev <imorenodev@gmail.com>
2016-03-08 18:07:16 -0500

    Added button to update and reset counter state

--- App.js
+++ App.js
@@ -2,23 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      val: 0
+    }
+    this.update = this.update.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+  update() {
+    this.setState({
+      val: this.state.val + 1
+    })
+  }
+  reset() {
+    this.setState({
+      val: 0
+    })
+  }
   render() {
+    console.log('rendering!');
     return (
-      <div>
-        <Button>I <Heart/> React</Button>
+      <div className="container" style={{'textAlign': 'center'}}>
+        <button className="btn btn-lg btn-danger" style={{'padding': '10px 40px 10px 40px','marginTop':'10px'}} onClick={this.update}>{this.state.val}</button>
+        <button className="btn btn-sma btn-warning" style={{'display':'block','margin':'10px auto'}} onClick={this.reset}>reset</button>
       </div>
     );
   }
 }
-
-class Button extends React.Component {
-  render() {
-    return (
-      <button className="btn btn-lg">{this.props.children}</button>
-    )
-  }
-}
-
-const Heart = () => <span className="glyphicon glyphicon-heart"></span>
 
 export default App;
